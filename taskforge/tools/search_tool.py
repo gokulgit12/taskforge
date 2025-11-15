ea7051425d0e4c82c6cf4e87bb216da4c564279a
gokulgit12 <testgot25@gmail.com>
2025-11-15 22:36:46 +0530

    Delete taskforge directory

--- taskforge/tools/search_tool.py
+++
@@ -1,5 +0,0 @@
-import time
-class MockSearchTool:
-    def call(self, query: str, top_k: int = 3):
-        time.sleep(0.2)
-        return [{'title': f'Result {i+1} for {query}', 'snippet': f'Snippet {i+1}'} for i in range(top_k)]
